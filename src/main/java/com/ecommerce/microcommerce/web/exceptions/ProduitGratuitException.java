44fd21b2346c8c38a0077c468a3f16bb6b8cce77
kvalifrontoffice <najib.aitelhouss@gmail.com>
2019-04-15 00:09:31 +0100

    Commit de la Partie 3 - Validation du prix de vente

---
+++ src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitGratuitException.java
@@ -0,0 +1,15 @@
+package com.ecommerce.microcommerce.web.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by Najib on 14/04/2019.
+ */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ProduitGratuitException extends RuntimeException {
+
+    public ProduitGratuitException(String s){
+        super(s);
+    }
+}
